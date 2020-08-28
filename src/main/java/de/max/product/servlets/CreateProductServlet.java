b33cb63fbb22726d719708951931ac4922a9122b
Max <max.gruber@huk-coburg.de>
2020-08-28 14:46:04 +0200

    Initial commit

---
+++ src/main/java/de/max/product/servlets/CreateProductServlet.java
@@ -0,0 +1,13 @@
+package de.max.product.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet
+public class CreateProductServlet extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+}
